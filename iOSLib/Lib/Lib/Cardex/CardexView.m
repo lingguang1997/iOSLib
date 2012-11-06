9af5db7ae6603b8fe2c4701cfa8b5bd303451d72
lingguang1997 <lingguang1997@gmail.com>
2012-11-06 13:37:55 -0500

    iOSLib
    - removed the IDE auto-generated text

--- iOSLib/Lib/Lib/Cardex/CardexView.m
+++ iOSLib/Lib/Lib/Cardex/CardexView.m
@@ -213,16 +213,6 @@ void *cardexIndexKey;
     }
 }
 
-//---------
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect
- {
- // Drawing code
- }
- */
-
 #pragma mark - private methods
 
 - (void)reset {
